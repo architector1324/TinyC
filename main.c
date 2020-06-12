72d309936087847d2b83a550710e8e3e392b3cbb
architector1324 <olegsajaxov@yandex.ru>
2020-06-13 00:55:40 +0300

    thrd: boilerplate

--- main.c
+++ main.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
+#include <unistd.h>
 
 // #include "img.h"
 // #include "mat.h"
 // #include "uni.h"
 // #include "rt.h"
-#include "vec.h"
+// #include "vec.h"
+#include "thrd.h"
+
+THREAD(void*, foo, float a) {
+    for(size_t i = 0; i < 3; i++) {
+        srand(time(NULL));
+
+        printf("%f\n", arg->a);
+        sleep(rand() % 2);
+    }
+}
 
 int main() {
-    vec(float) a = vec_init(float)();
-    vec_reserve(a, 6);
+    thrd(foo) th0;
+    thrd(foo) th1;
 
-    for(size_t i = 0; i < 6; i++) {
-        printf("size=%ld cap=%ld\n", a.size, a.cap);
-        vec_push(a, 3.14f);
-    }
-
-    vec_reserve(a, 8);
-    printf("size=%ld cap=%ld\n", a.size, a.cap);
-
-    vec_reserve(a, 7);
-    printf("size=%ld cap=%ld\n", a.size, a.cap);
-
-    vec_reserve(a, 3);
-    printf("size=%ld cap=%ld\n", a.size, a.cap);
-
-    vec_free(a);
+    thrd_create(foo, th0, 3.14f);
+    thrd_create(foo, th1, 2.71f);
+    
+    thrd_join(th0);
+    thrd_join(th1);
 
     // // scene
 
--- thrd.h
+++ thrd.h
@@ -2,5 +2,44 @@
 #define THRD
 
 #include <pthread.h>
+#include <stdlib.h>
+
+// extra
+#define __cat(X, Y) X##Y
+#define _cat(X, Y) __cat(X, Y)
+
+#define thrd(name) _cat(_thrd_, name)
+#define thrd_args(name) _cat(_thrd_args_, name)
+#define thrd_init(name) _cat(thrd(name), _init)
+
+#define thrd_create(name, th, args...) _cat(thrd(name), _create)(&th, (thrd_args(name)){args})
+#define thrd_join(th) th.join(&th)
+
+#define THREAD(type, name, args...)\
+typedef struct _cat(__thrd_args_, name) {\
+    args;\
+} thrd_args(name);\
+typedef struct _cat(__thrd_, name) {\
+    pthread_t th;\
+    thrd_args(name) arg;\
+    type res;\
+    type (*join)(struct _cat(__thrd_, name)*);\
+} thrd(name);\
+type name(const thrd_args(name)* arg);\
+void* _cat(thrd(name), _bootstrap)(void* raw) {\
+    thrd(name)* th = (thrd(name)*)(raw);\
+    th->res = name(&th->arg);\
+    return th;\
+}\
+type _cat(thrd(name), _join)(thrd(name)* th){\
+    pthread_join(th->th, NULL);\
+    return th->res;\
+}\
+void _cat(thrd(name), _create)(thrd(name)* th, thrd_args(name) arg){\
+    th->arg = arg;\
+    th->join = _cat(thrd(name), _join);\
+    pthread_create(&th->th, NULL, _cat(thrd(name), _bootstrap), th);\
+}\
+type name(const thrd_args(name)* arg)
 
 #endif
